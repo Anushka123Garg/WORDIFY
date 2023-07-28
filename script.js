61638813575a747726297f129fa911a2baea4314
Anushka Garg <anushkagarg2912@gmail.com>
2023-07-28 20:10:53 +0530

    phonetic

--- script.js
+++ script.js
@@ -9,55 +9,67 @@ const dictionary = document.querySelector('.dictionary')
 async function dictionaryFn(word) {
     const resp = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then(resp => resp.json())
-    //in json format
+    //converting to json format
 
-    // console.log(resp)
+    console.log(resp)
     return resp[0]
 }
 
-btn.addEventListener('click', fetch_createcard) //on clicking button
+// btn.addEventListener('click', fetch_createcard); //only onclicking button
+
+//on clicking button and pressing enter
+input.addEventListener('keyup', e=>{
+    e.preventDefault();
+    if(e.keyCode === 13){
+        console.log("enter is pressed");
+        btn.click();
+    }
+});
+btn.addEventListener('click', ()=>{
+    fetch_createcard();   
+});
 
 async function fetch_createcard() {
     const data = await dictionaryFn(input.value) //whatever is the input work on that input value
     console.log(data)
 
-    let partofSpeech = []
+    let partofSpeechArr = []
 
     for (let i = 0; i < data.meanings.length - 1; i++) {
-        partofSpeech.push(data.meanings[i].partOfSpeech)
+        partofSpeechArr.push(data.meanings[i].partOfSpeech)
     }
 
     dictionary.innerHTML = `
     <div class="card"> 
                 <div class="property">
-                    <font size=6px><b><span>${data.word}</span></b></font>
+                    <font size=6px><b><span>${data?.word}</span></b></font> 
                 </div>
 
                 <div class="property">
                     <span>Phonetics:</span> 
-                    <span>${data.phonetics}</span> 
+                    <span>${data?.phonetic}</span> 
                 </div>
 
                 <div class="property">
                     <span>
-                    <audio controls src="${data.phonetics[0].audio}"></audio>
+                    <audio controls src="${data?.phonetics[0]?.audio}"></audio>
                     </span>
                 </div>
 
                 <div class="property">
                     <span>Definition:</span> 
-                    <span>${data.meanings[0].definitions[0].definition}</span>  
+                    <span>${data?.meanings[0]?.definitions[0]?.definition}</span>  
                 </div>
 
                 <div class="property">
                     <span>Example:</span>
-                    <span>${data.meanings[0].definitions[0].example}</span> 
+                    <span>${data?.meanings[0]?.definitions[1]?.example}</span> 
                 </div>
 
                 <div class="property">
-                    <span>${partofSpeech.map(e => e)}</span> 
+                    <span>Parts of Speech:</span>
+                    <span>${partofSpeechArr.map(e => e).join(',')}</span> 
                 </div>
             </div>`
 }
-
-dictionaryFn('chair') //function call
+dictionaryFn() //function call
